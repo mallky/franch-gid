a943db0f4a8f96a71e07606458dfd9f07ece85ed
MKuzmichev <MKuzmichev@competentum.ru>
2018-04-08 14:30:03 +0400

    (feature) add carousel

--- src/blocks/when-buy/when-buy.js
+++ src/blocks/when-buy/when-buy.js
@@ -1,11 +1,26 @@
 import './when-buy.less';
 import { Column } from '../../../utils/utils';
+import $ from 'jquery';
+
+import BecomeOwner from '../../components/become-owner/become-owner';
 
 const whenBuy = require('./when-buy.html');
 
 export default class WhenBuy extends Column {
   init () {
     super.init(whenBuy);
+
+    const becomeOwner = new BecomeOwner(this.root.querySelector('#become-owner'));
+
+    becomeOwner.init();
+    
+    this.showForm();
+  }
+
+  showForm () {
+    $('#get-presentation-btn').click( () => {
+      $('#getPresentationModal').modal('show');
+    });    
   }
 }
 
---
+++ src/components/become-owner/become-owner.js
@@ -0,0 +1,23 @@
+import './become-owner.less';
+import { Column } from '../../../utils/utils';
+import $ from 'jquery';
+
+const becomeOwner = require('./become-owner.html');
+
+export default class BecomeOwner extends Column {
+  init () {
+    super.init(becomeOwner);
+
+    this.changeImage();
+  }
+
+  changeImage() {
+    $(window).on('load resize', () => {
+      const imgSrc = $(window).width() <= 993
+        ? '../../../images/transformed-arrow-icons.png' : '../../../images/arrow-icons.png';
+      $('.image-container').find('.man-img')
+        .find('img:last-child').attr('src', imgSrc);
+    });
+  }
+}
+
