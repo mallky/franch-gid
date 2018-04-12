b72d1be02f6343e252a60865013098aeedb36cea
mallky <makarkuz@yandex.ru>
2018-04-12 21:49:42 +0400

    (fix) fix map

--- src/blocks/map/map.js
+++ src/blocks/map/map.js
@@ -1,10 +1,15 @@
 import './map.less';
 import { Column } from '../../../utils/utils';
+import $ from 'jquery';
 
 const map = require('./map.html');
 
 export default class Map extends Column {
   init () {
     super.init(map);
+
+    $('#map').find('button').click(() => {
+      $('#getPresentationModal').modal('show');
+    });
   }
 }
