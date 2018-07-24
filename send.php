e0c8c1a4d55b375071e24b682c8677bf429d4a7d
mallky <makarkuz@yandex.ru>
2018-07-24 18:51:37 +0400

    fix instruction, change  contacts

--- send.php
+++ send.php
@@ -1,6 +1,6 @@
 <?php
 if (isset($_POST['phone'])) {
-    $to  = "makarkuz@yandex.ru";
+    $to  = "matveev@gstamp.ru";
     $subject = isset($_POST['email']) ? "Заказ презентации и гарантий" : "Заказ звонка";
     $message = isset($_POST['email'])
         ? "Имя заказчика: ".$_POST['name']."<br/>Телефон заказчика: ".$_POST['phone']."<br/> Email: ".$_POST['email']
